3b6345d3aed18e2e228073dd181c1cadde94b316
RowBowl <robelrb@gmail.com>
2023-09-19 15:48:42 -0400

    Comments on design decisions and future plans

--- app/src/main/java/com/example/wellnessapp/MainActivity.kt
+++ app/src/main/java/com/example/wellnessapp/MainActivity.kt
@@ -6,11 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.wellnessapp.ui.theme.WellnessAppTheme
+
+
+/** App Design Notes
+ * Theme: Wellness (encompasses fitness, physical and mental health)
+ * The app will have a collection of motivational cards. Each card contains
+ * text for the title and body. The body will be a short 2-3 paragraph description.
+ * Instead of an image, I will have a background simple icon or image that relates
+ * to the contents of the card.
+ *
+ * Card takes up most of the screen. Swiping up or down reveals new cards (much like tiktoks)
+ *
+ * can experiment with shader gradient backgrounds for cards
+ *
+ * can make it so that the list snaps to the "current" day item on app open
+ *
+ * use LazyColumn for vertical scrolling list
+ *
+ * test using dark theme preview ****
+ *
+ * Font: Using very light font for message body (Roboto light). Using Nunito Sans for title
+ *
+ * Theme: Chosen wth material theme builder
+ *
+ * Shapes: Rounded corner outline card for the "view port" . Within this view port, dragging up
+ * or down scrolls the lazy column up or down respectivly. Actual list items are also cards that
+ * fit perfectly on the "view port".
+ *
+ * Animations: After all parts of the design are programmed into the app, I will add animations
+ * to expand or collapse the card element information. Or maybe a "tap to reveal" feature.
+ *
+ *
+ */
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
