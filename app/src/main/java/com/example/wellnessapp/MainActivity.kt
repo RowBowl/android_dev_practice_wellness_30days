955e7a61fa6a2216e7fae2b2b1aa330b7e238e9e
RowBowl <robelrb@gmail.com>
2023-09-27 17:25:59 -0400

    fixed snapping behavior when list was collapsed.

--- app/src/main/java/com/example/wellnessapp/MainActivity.kt
+++ app/src/main/java/com/example/wellnessapp/MainActivity.kt
@@ -5,10 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapLayoutInfoProvider
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,15 +17,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowDropDown
-import androidx.compose.material.icons.outlined.Check
-import androidx.compose.material.icons.outlined.Star
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -44,11 +36,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import com.example.wellnessapp.ui.theme.WellnessAppTheme
 
@@ -213,7 +205,16 @@ fun MotivationalList(currentCardState: ExpandableCardState,
                      modifier: Modifier = Modifier) {
 
     val listState = rememberLazyListState()
-    val flingBehavior = rememberSnapFlingBehavior(lazyListState = listState)
+
+    //https://stackoverflow.com/questions/71901039/snap-to-an-index-lazyrow/77005797#77005797
+    val positionInLayout: Density.(Int, Int, Int) -> Int = { _, _, _ ->
+        // This value tells where to snap on the x axis within the viewport
+        // Setting it to 0 results in snapping of the first visible item to the left side (or right side if RTL)
+        0
+    }
+    val snappingLayout = remember(listState) { SnapLayoutInfoProvider(lazyListState = listState, positionInLayout = positionInLayout) }
+    val flingBehavior = rememberSnapFlingBehavior(snappingLayout)
+
 
     LazyColumn(
         state = listState,
@@ -221,7 +222,7 @@ fun MotivationalList(currentCardState: ExpandableCardState,
             .scale(if (listState.isScrollInProgress && currentCardState.isExpanded) 0.9f else 1f)
             .animateContentSize(),
         flingBehavior = flingBehavior,
-        verticalArrangement = Arrangement.spacedBy(6.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
     ) {
         items(30) {
             MotivationalCard(
@@ -251,7 +252,7 @@ fun MotivationalCard(currentState: ExpandableCardState, modifier: Modifier = Mod
             modifier = Modifier
         ) {
             Row (
-                modifier = Modifier.height(100.dp)
+                modifier = Modifier.height(80.dp)
             ) {
                 Spacer(modifier = Modifier
                     .fillMaxSize()
